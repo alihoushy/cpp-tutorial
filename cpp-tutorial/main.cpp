4074f449cf4bb0291eab05146b978cac20488e7f
Ali Houshyari Pour <alihoushyaripour@gmail.com>
2024-12-10 20:32:26 +0330

    Initial Commit

---
+++ cpp-tutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-tutorial
+//
+//  Created by Ali Houshyaripour on 9/20/1403 AP.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
